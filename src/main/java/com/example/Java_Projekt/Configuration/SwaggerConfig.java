b1216d16aaa8dc1621ef7f0e933a08aaaf11d5ab
KacperGebusia <gebusiakacper@gmail.com>
2023-11-14 20:47:31 +0100

    Feat: Dodanie uwierzytelniania w Swagger

--- src/main/java/com/example/Java_Projekt/Configuration/SwaggerConfig.java
+++ src/main/java/com/example/Java_Projekt/Configuration/SwaggerConfig.java
@@ -1,5 +1,11 @@
 package com.example.Java_Projekt.Configuration;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -18,7 +24,22 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .build();
     }
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("Java_projekt")
+                        .description("Zaawansowane programowanie w języku JAVA.")
+                        .version("1.0").contact(new Contact().name("Kacper Gębusia, Bartłomiej Głuszczak")));
+    }
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html");
+    }
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
